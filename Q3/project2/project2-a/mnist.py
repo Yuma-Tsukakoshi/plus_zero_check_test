c455790da7cfb9dd6204a221e25ab980055beaae
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-07-31 12:36:04 +0900

    add: modules import

--- Q3/project2/project2-a/mnist.py
+++ Q3/project2/project2-a/mnist.py
@@ -1,2 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+#GPUの利用確認
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+
 
